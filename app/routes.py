3a9a6e74db4c9c0f243ee274267b6a7983a62236
kmi0817 <kmi0817@naver.com>
2022-03-30 15:40:39 +0900

    feat: :truck: add /index route (temporary)

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from app.models import Users, Postings, Comments
 from flask import render_template, redirect, url_for, session, request, jsonify
 import sys
 
+@app.route('/index')
+def index() :
+    return render_template('index.html')
+    
 @app.route('/')
 def home() :
     ret = False
