d1715a8382aea9c7b6905edb692606282ef36e63
kmi0817 <kmi0817@naver.com>
2022-04-02 22:42:11 +0900

    perf: :children_crossing: Print 12 repos at first
    First 12 repositories will be shown in the page, and when the scroll reaches the bottom, every 12 repositories will be loaded.

--- app/routes.py
+++ app/routes.py
@@ -17,10 +17,16 @@ except :
     
 @app.route('/')
 def index() :
-    repos_path = os.getcwd() + '/app/static/repo_test.json'
+    repos_path = os.getcwd() + '/app/static/repo_list.json'
     with open(repos_path, 'r') as f :
         repos = json.loads(f.read())
-    return render_template('index.html', repos=repos)
+
+        # only first 12 repositories will be printed
+        repos_12 = []
+        for i in range(12) : # append 12 repositories
+            repos_12.append(repos['repo_' + str(i)])
+        repos_12 = { i: repos_12[i] for i in range(len(repos_12))} # list -> dict
+    return render_template('index.html', repos=repos_12)
     
 @app.route('/data')
 def data() :
