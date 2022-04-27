ad8031ca2f7a7b3cf371b2e5f421506c7f51214f
kmi0817 <kmi0817@naver.com>
2022-04-27 18:08:12 +0900

    refactor: join direcotry routes and  repo_name
    index.html에 repo_name 보내야 하므로, 아예 index()에서 if문으로 repo_name 값 할당하고, repos_path로 한 번에 파일 경로 완성했음.

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,20 @@ import os.path
 
 @app.route('/')
 def index() :
-    # sorting parameter별 가져올 파일 처리
+    # sorting parameter별 가져올 json 파일 처리
     if request.args.get('sortedBy') == None : # 디폴트: created_by
-        repos_path = os.getcwd() + '/app/static/repo_list.json'
+        repo_name = 'repo_list.json'
 
     elif request.args.get('sortedBy') == 'star' : # most stars
-        repos_path = os.getcwd() + '/app/static/repo_list.json'
+        repo_name = 'repo_list.json'
 
     elif request.args.get('sortedBy') == 'name' : # name ascending
-        repos_path = os.getcwd() + '/app/static/repo_list.json'
+        repo_name = 'repo_list.json'
 
+    # json 파일 경로 구성
+    repos_path = os.getcwd() + f'/app/static/{repo_name}'
+
+    # json 파일 내 12개의 repositories 정보만 가져옴
     with open(repos_path, 'r') as f :
         repos = json.loads(f.read())
         total = len(repos) # the number of repositories
@@ -24,7 +28,8 @@ def index() :
         for i in range(12) : # append 12 repositories
             repos_12.append(repos['repo_' + str(i)])
         repos_12 = { i: repos_12[i] for i in range(len(repos_12))} # list -> dict
-    return render_template('index.html', total=total, repos=repos_12, repos_path=repos_path)
+        
+    return render_template('index.html', total=total, repos=repos_12, repo_name=repo_name)
     
 @app.route('/webix')
 def data() :
