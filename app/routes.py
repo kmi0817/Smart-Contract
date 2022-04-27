fe25c61d4f44666c5c2872d0c4d7b57cabc83dce
kmi0817 <kmi0817@naver.com>
2022-04-27 17:46:49 +0900

    feat: 정렬 파라미터별로 가져올 파일 경로 제어
    index() 내에서 정렬 파라미터 종류에 따라 가져올 repo_list 파일 변경함.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, session
+from flask import render_template, session, request
 import json
 import pymongo
 import os.path
@@ -17,7 +17,16 @@ except :
     
 @app.route('/')
 def index() :
-    repos_path = os.getcwd() + '/app/static/repo_list.json'
+    # sorting parameter별 가져올 파일 처리
+    if request.args.get('sortedBy') == None : # 디폴트: created_by
+        repos_path = os.getcwd() + '/app/static/repo_list.json'
+
+    elif request.args.get('sortedBy') == 'star' : # most stars
+        repos_path = os.getcwd() + '/app/static/repo_list.json'
+
+    elif request.args.get('sortedBy') == 'name' : # name ascending
+        repos_path = os.getcwd() + '/app/static/repo_list.json'
+
     with open(repos_path, 'r') as f :
         repos = json.loads(f.read())
         total = len(repos) # the number of repositories
@@ -27,7 +36,7 @@ def index() :
         for i in range(12) : # append 12 repositories
             repos_12.append(repos['repo_' + str(i)])
         repos_12 = { i: repos_12[i] for i in range(len(repos_12))} # list -> dict
-    return render_template('index.html', total=total, repos=repos_12)
+    return render_template('index.html', total=total, repos=repos_12, repos_path=repos_path)
     
 @app.route('/data')
 def data() :
